5255b9a154ef47e5c2f0d4f6a5cc0a903c9389c2
Kayla Nimis <kaylanimis@gmail.com>
2016-07-17 17:52:27 -0500

    Use some sweet reactive bindings

--- KN.Reactive/MainViewModel.cs
+++ KN.Reactive/MainViewModel.cs
@@ -1,7 +1,30 @@
-﻿namespace KN.Reactive
+﻿using System;
+using System.Reactive.Subjects;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace KN.Reactive
 {
-    public class MainViewModel
+    public class MainViewModel : ReactiveObject
     {
+        private int _currQty = 0;
+        private readonly IObserver<int> _qty = new Subject<int>();
+        private readonly ReactiveCommand _command;
+        public MainViewModel()
+        {
+            var commandCanExecute = this.WhenAny(x => x.Quantity, x => x.Value < 15);
+            _command = new ReactiveCommand(commandCanExecute);
+            _command.Subscribe(x => _qty.OnNext(Quantity++));
+        }
+
         public string HelloText => "Hello, World!";
+
+        public ICommand Command => _command;
+
+        public int Quantity
+        {
+            get { return _currQty; }
+            set { this.RaiseAndSetIfChanged(ref _currQty, value); }
+        }
     }
 }
--- KN.Reactive/MainWindow.xaml.cs
+++ KN.Reactive/MainWindow.xaml.cs
@@ -12,18 +12,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ReactiveUI;
 
 namespace KN.Reactive
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IViewFor<MainViewModel>
     {
         public MainWindow(MainViewModel viewModel)
         {
             InitializeComponent();
-            DataContext = viewModel;
+            DataContext = ViewModel = viewModel;
+
+            this.WhenActivated(GenerateBindings);
         }
+
+        private IEnumerable<IDisposable> GenerateBindings()
+        {
+            return new[]
+            {
+                this.Bind(ViewModel, x => x.HelloText, x => x.HelloWorldLabel.Content),
+                this.Bind(ViewModel, x => x.Command, x => x.IncrementButton.Command),
+                this.Bind(ViewModel, x => x.Quantity, x => x.CounterTextBlock.Text)
+            };
+        }
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (MainViewModel) value; }
+        }
+
+        public MainViewModel ViewModel { get; set; }
     }
 }
